398fa1843d6b15ed380dfa40d845dae351506217
ribeiroarthur <56894765+ribeiroarthur@users.noreply.github.com>
2019-12-07 21:37:40 -0300

    parte final 02

--- main.cs
+++ main.cs
@@ -1,145 +1,108 @@
+
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
+
 
 class MainClass {
+   
   public static void Main (string[] args) {
 
-    int opcao = 0 ;
-    int senha = 0 ;
+  Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 
-    Fabrica fabrica = new Fabrica();
-
-    Estoque est = new Estoque();
-    fabrica.AssociarEstoque(est);
-
-    Console.WriteLine("Digite a senha para entrar no Sistema:");
-    senha =int.Parse(Console.ReadLine());
-
-    if( senha != 1234){
-      Console.ForegroundColor = ConsoleColor.Red;
-      Console.WriteLine("Senha invalida !!!\n Não é possivel entrar no sistema.");
-    }
   
-    while (opcao == 1 || senha == 1234 || opcao == 2 || opcao == 3){
+  Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("========================================================  MEXICANAS  ==========================================================");
 
-    Console.WriteLine(" Digite 4 para sair:");
+  Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+  Console.ForegroundColor = ConsoleColor.White; 
+
+  Console.WriteLine();
+
+
+  Console.WriteLine(" Para entrar no nosso sistema: \n Informe se você é uma pessoa fisica ou uma pessoa juridica:");
+
+  Console.WriteLine();
+
+  Console.WriteLine(" Digite 1 para Pessoa fisica\n =============================\n Digite 2 para Pessoa Juridico");
+  string resp  = (Console.ReadLine());
+
+
+  Console.WriteLine();
+
+   int opcao = 0;
+  
+
+  while(resp == "1" || opcao == 1 || opcao == 2){
+
+    Console.WriteLine("Digite 1 para fazer um cadastro no nosso Sistema:\nDigite 2 se você já é cadastrado:");
+    opcao = int.Parse(Console.ReadLine());
+
+
+     
+    if (opcao == 1){
 
     
-    Console.WriteLine(" Digite 1 para fabricar um produto:\n Digite 2 para entrar no sistema de estoque:\n Digite 3 para pedir produto do estoque:");
-    opcao =int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite seu nome para cadastro:");
+    string nome  = (Console.ReadLine());
+
+    Console.WriteLine("Digite seu email para cadastro:");
+    string  email = (Console.ReadLine());
    
-    if (opcao == 1 ){
-    Console.WriteLine("Digite o código do produto para ser fabricado: ");
-    string codigo = Console.ReadLine();
+  
+    Console.WriteLine("Digite seu cep:");
+    string  cep = (Console.ReadLine());
 
-    if(fabrica.VerificarCodigo(est.prods,codigo)){
+    Console.WriteLine("Digite sua senha para cadastrar:");
+    string  senha  = (Console.ReadLine());
 
-      Console.WriteLine("Digite o tipo do produto para ser fabricado: ");
-      string tipo = Console.ReadLine();
+    Console.WriteLine("Digite sua senha para cadastrar:");
+    string  cpf  = (Console.ReadLine());
 
-      Console.WriteLine("Digite o tamanho em número:");
-      int tamanho = int.Parse(Console.ReadLine());
-
-      Console.WriteLine("Digite a descrição do produto: ");
-      string descrição = Console.ReadLine();
-
-      Console.WriteLine("Digite o custo de cada produto para ser fabricado: ");
-      float custo = float.Parse(Console.ReadLine());
-
-      Console.WriteLine("Digite a quantidade:");
-      int quantidade = int.Parse(Console.ReadLine());
-
-      Console.WriteLine("Valor desejado para venda:");
-      int valor_desejada = int.Parse(Console.ReadLine());
-
-      
-
-      Produto produto = fabrica.Criar_Produto(codigo,tipo,tamanho,descrição,custo,quantidade);
-      
-      Console.WriteLine(produto.ResumoDados());
-
-    }
-    else{
-      Console.ForegroundColor = ConsoleColor.Red;
-      Console.WriteLine("código já existe!!!\n Tente novamente!!");
-    }
     
-    }  
-    if (opcao == 2){
-      int op = 0;
+    Fisico  usuario = new Fisico(nome,email,cep,senha,cpf);
 
-      Console.WriteLine("Digite a opção para analisar o estoque:");
+    usuario.CadastroUsuarioFisico(nome, email,cep,senha,cpf);
+    Console.ForegroundColor = ConsoleColor.White; 
+    break;
+      }
+
+
+    if(opcao == 2){
+
+      Fisico  usuario = new Fisico();
+
+      Console.WriteLine("Digite seu email para cadastro:");
+      string  email = (Console.ReadLine());
+
+      Console.WriteLine("Digite seu email para cadastro:");
+      string  senha = (Console.ReadLine());
+
       Console.WriteLine();
 
-      Console.WriteLine(" opção 1 = analisar todos os produtos em estoque:\n opção 2 = analisar a quantidade total de produtos em estoque: \n opção 3 =  analisar custo total em estoque: ");
-       op = int.Parse(Console.ReadLine());
+      usuario.Login(email,senha); 
 
-      if (op == 1){
-        Console.WriteLine("========================");
-        est.MostrarProduto();
-        Console.WriteLine("========================");
+      int opcaoUsuario = 0;
+
+      Console.WriteLine(" Vamos lá.\nDigite 1 para poder fazer uma compra\n Digite 2 para verficar seu historico de compra em nossa loja:");
+      opcaoUsuario = int.Parse(Console.ReadLine());
+
+      if(opcaoUsuario == 2){
+        HistoricoFisico historico = new HistoricoFisico();
+        Console.WriteLine("Esse é todo o seu historico em nossa loja");
+
+
+
+        historico.MostrarHistorico(email);
       }
 
-      if(op == 2){
-        Console.WriteLine("O total de produtos em estoque: {0} ",est.SomarQuantidade());
-      }
 
-      if (op == 3){
-        Console.WriteLine("O valor total em estoque: R${0}",est.SomarCusto());
-      }
-    }
-
-    if ( opcao == 3 ){
-    est.MostrarProduto();
-
-    Console.WriteLine("Buscar no estoque por codigo:");
-    string dadoCodigo = Console.ReadLine();
-    est.BuscarEstoque(dadoCodigo);
-
-    Console.WriteLine("Digite as informação para compra:");  
-
-    Console.WriteLine(" digite a quantidade de produtos para venda:");
-    int quantidadeCompra = int.Parse(Console.ReadLine());
-    est.removerestoque(dadoCodigo,quantidadeCompra);
-
-    Console.WriteLine("Digite o nome do cliente:");
-    string nome = Console.ReadLine();
-
-    Console.WriteLine("Digite o email do cliente:");
-    string email = Console.ReadLine();
-
-    Console.WriteLine("Digite o nome da empresa:");
-    string empresa = Console.ReadLine();
-
-    Console.WriteLine("Valor desejado para venda:");
-    int valor_desejada = int.Parse(Console.ReadLine());
-
-    VenderProd venderproduto = new VenderProd(quantidadeCompra, nome , email, empresa,valor_desejada);
-
-    Console.WriteLine(venderproduto.MostraCompra());
-
-    venderproduto.ControleDeVendas(email,empresa,nome,quantidadeCompra,valor_desejada);
-
- 
-    }  
-    
-    if (opcao == 4){
-      File.Delete("compras.txt");
-      
-      break;
     }
     
-    Console.WriteLine("Vamos limpar a tela aperte enter:");
     
-    Console.ReadLine();
-      Console.Clear();     
      
-  }
-
-  }
-
+          
+    }
+  }   
 }
-  
- 
